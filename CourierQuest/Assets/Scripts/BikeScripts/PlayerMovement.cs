165e70d467f67b6cca498f21512fddf4a70c7312
dhe077 <dhe077@aucklanduni.ac.nz>
2023-10-31 16:10:59 +1300

    Bike Movement Test Implementation 1
    - this push is for the lab pc with the bike

---
+++ CourierQuest/Assets/Scripts/BikeScripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float bikeSpeed = 0.0f;
+    public int bikeRPM = 0;
+    public int bikeHeartRate = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReadBikeData()
+    {
+        
+    }
+}
