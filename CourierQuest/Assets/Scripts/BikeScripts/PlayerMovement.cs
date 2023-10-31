6adb275fcddc6effbd97663e1b86db545fb8b0a1
dhe077 <dhe077@aucklanduni.ac.nz>
2023-10-31 18:15:43 +1300

    Bike Movement Working
    - the PlayerMovement can now be controlled by the bike pedalling
    - make sure to turn on in this order
    1. Turn on game
    2. Clear any workouts
    3. Pedal a little to finish the set up
    4. Press the start button on the machine

--- CourierQuest/Assets/Scripts/BikeScripts/PlayerMovement.cs
+++ CourierQuest/Assets/Scripts/BikeScripts/PlayerMovement.cs
@@ -1,27 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public BikeController bikeController;
     public float bikeSpeed = 0.0f;
     public int bikeRPM = 0;
     public int bikeHeartRate = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI speedText;
+    public TextMeshProUGUI rpmText;
+    public TextMeshProUGUI heartRateText;
+
+    public float moveSpeed = 10.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        ReadBikeData();
+        Debug.Log($"Speed: {bikeSpeed}");
+        speedText.text = $"Speed: {bikeSpeed}";
+
+        Debug.Log($"RPM: {bikeRPM}");
+        rpmText.text = $"RPM: {bikeRPM}";
+
+        Debug.Log($"Heart rate: {bikeHeartRate}");
+        heartRateText.text = $"Heart rate: {bikeHeartRate}";
     }
 
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    // This function pulls the data from the bike controller class
     public void ReadBikeData()
     {
-        
+        bikeSpeed = bikeController.speed;
+        bikeRPM = bikeController.RPM;
+        bikeHeartRate = bikeController.heartRate;
+    }
+
+    // To test the speed of movement.
+    public void MovePlayer()
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, bikeSpeed) * moveSpeed * Time.deltaTime;
+
+        // Move the player
+        transform.Translate(movement);
     }
 }
