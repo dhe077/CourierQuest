bfad45d9821b941c519091c7df064f05a1defd50
dhe077 <69486294+dhe077@users.noreply.github.com>
2023-11-06 16:51:25 +1300

    ObstacleForestRoad Scene 1
    - beginning ObstacleForestRoad scene
    - created PathGenerator object to generate paths ahead of the player using a new script
    - created new script ForestPathGeneration, generates paths ahead of player

---
+++ CourierQuest/Assets/Scripts/ForestPathGeneration.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ForestPathGeneration : MonoBehaviour
+{
+    [SerializeField] public GameObject playerView;
+    [SerializeField] public List<GameObject> forestPathPrefabs;
+    //[SerializeField] public GameObject forestPathSplit;
+
+    [SerializeField] public Queue<GameObject> currentForestPaths;
+    // This is the x position of the prefabs when intantiating them
+    // Should increase by 60 every time
+    private int xPosition = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the player view game object
+        playerView = GameObject.Find("PlayerView");
+
+        // Initialise queue
+        currentForestPaths = new Queue<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerView.transform.position.x >= xPosition - 180)
+        {
+            GeneratePath();
+            GeneratePath();
+            GeneratePath();
+        }
+        if (currentForestPaths.Count > 8)
+        {
+            DestroyPath();
+            DestroyPath();
+            DestroyPath();
+        }
+    }
+
+    private int ChooseForestPath()
+    {
+        System.Random rnd = new System.Random();
+        int pathIndex = rnd.Next(0, forestPathPrefabs.Count);
+        return pathIndex;
+    }
+
+    private void GeneratePath()
+    {
+        // Choose path prefab
+        int pathIndex = ChooseForestPath();
+        // Get next path from list
+        GameObject nextPath = forestPathPrefabs[pathIndex];
+        // Make a Vector3 for the next position and move xPosition forward 60 and 
+        Vector3 nextPosition = new Vector3(xPosition, 0, 0);
+        xPosition += 60;
+        Quaternion noRotation = Quaternion.identity;
+        // Instantiate path at next position
+        GameObject newPath = Instantiate(nextPath, nextPosition, noRotation);
+        currentForestPaths.Enqueue(newPath);
+    }
+
+    private void DestroyPath()
+    {
+        GameObject oldPath = currentForestPaths.Dequeue();
+        Destroy(oldPath);
+    }
+}
