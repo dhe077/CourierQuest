f31d9959b2866336d1a02f8cb7d09555bb8b29c2
dhe077 <69486294+dhe077@users.noreply.github.com>
2024-01-31 12:16:45 +1300

    Added characters to scenes 16
    - added LavaGuardian to scenes (InsideVolcanoBeast)

--- CourierQuest/Assets/Scripts/TestScripts/SceneSelector.cs
+++ CourierQuest/Assets/Scripts/TestScripts/SceneSelector.cs
@@ -225,7 +225,7 @@ public class SceneSelector : MonoBehaviour
         //variables.SetValue("$axe", true);
         //variables.SetValue("$lantern", true);
         //variables.SetValue("$magicalCompass", true);
-        //variables.SetValue("$assassin", true);
+        variables.SetValue("$assassin", true);
 
         dialogueRunner.StartDialogue(nodeName);
     }
